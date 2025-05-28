d4cc9551f583a86e885736fdbbdc2578b8369e43
Nikola Berankova <nikoneecha@gmail.com>
2025-05-28 15:45:32 +0200

    chore: responsiveness of cv page

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -10,13 +10,19 @@ export default function CV() {
       <Header />
 
       <main className="flex-1 pb-15">
-        <div className="flex flex-row justify-center gap-15 bg-[#fff0f5] rounded-xl py-10 w-[1500px] mx-auto">
-          <div className="flex flex-col justify-between gap-auto">
-            <Education />
-            <Extra />
+        <div className="grid xl:grid-cols-2 md:gap-5 lg:gap-8 xl:gap-12 2xl:gap-16 mx-auto sm:max-w-[750px] xl:max-w-[1500px]">
+          <div className="bg-[#fff0f5] rounded-xl py-10 px-5 h-full">
+            <div className="flex flex-col justify-between gap-5 h-full items-center">
+              <Education />
+              <Extra />
+            </div>
           </div>
 
-          <Experience />
+          <div className="bg-[#fff0f5] rounded-xl py-10 px-5 h-full">
+            <div className="flex flex-col justify-between gap-5 h-full items-center">
+              <Experience />
+            </div>
+          </div>
         </div>
       </main>
 
