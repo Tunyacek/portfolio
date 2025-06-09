e115d2766481cdb8251135e333e84421c333ebc2
Nikola Berankova <nikoneecha@gmail.com>
2025-06-09 17:06:56 +0200

    feat: responsiveness of cv page

--- src/components/cv/ed.tsx
+++ src/components/cv/ed.tsx
@@ -1,29 +1,38 @@
 export default function Education() {
   return (
-    <div className="w-150 border-2 border-dotted border-black rounded-lg h-auto ">
+    <div className="w-full max-w-[600px] border-2 border-dotted border-black rounded-lg h-auto ">
       <div className="p-5 flex flex-col gap-2">
         <p className="font-bold text-2xl ">Education</p>
-        <p> 🦜Exotic animal breeder SOŠ a SOU Čakovice 2013 - 2017 🐆</p>
-        <hr className="border-dotted border-black w-125 mx-auto mt-2" />
-        <p className="font-bold text-2xl">Skills</p>
+        <p className="text-sm md:text-md">
+          {" "}
+          🦜Exotic animal breeder SOŠ a SOU Čakovice 2013 - 2017 🐆
+        </p>
+        <hr className="border-dotted border-black w-full max-w-[500px] mx-auto mt-2" />
+        <p className="font-bold text-xl sm:text-2xl">Skills</p>
         <ol>
-          <li>✨Frontend Development: React and TypeScript.</li>
-          <li>✨Backend Development: Express and Prisma.</li>
-          <li>✨Database Management: Prisma ORM and Supabase.</li>
-          <li>
+          <li className="text-sm md:text-md">
+            ✨Frontend Development: React and TypeScript.
+          </li>
+          <li className="text-sm md:text-md">
+            ✨Backend Development: Express and Prisma.
+          </li>
+          <li className="text-sm md:text-md">
+            ✨Database Management: Prisma ORM and Supabase.
+          </li>
+          <li className="text-sm md:text-md">
             ✨Full-Stack Development: Created a recipe app with features like
             user authentication, CRUD operations, and search functionality.
           </li>
-          <li>
+          <li className="text-sm md:text-md">
             ✨Self-Learner: Learned React, TypeScript, Express, and Prisma
             through online tutorials and selfguided project.
           </li>
         </ol>
-        <hr className="border-dotted border-black w-125 mx-auto mt-2" />
-        <p className="font-bold text-2xl">Language</p>
+        <hr className="border-dotted border-black w-full max-w-[500px] mx-auto mt-2" />
+        <p className="font-bold text-xl sm:text-2xl">Language</p>
         <ol>
-          <li>✨Czech</li>
-          <li>✨English</li>
+          <li className="text-sm md:text-md">✨Czech</li>
+          <li className="text-sm md:text-md">✨English</li>
         </ol>
       </div>
     </div>
--- src/components/cv/exp.tsx
+++ src/components/cv/exp.tsx
@@ -2,15 +2,15 @@ import AboutMe from "./about-me";
 
 export default function Experience() {
   return (
-    <div className="w-150 border-2 border-dotted border-black rounded-lg">
+    <div className="w-full max-w-[600px] border-2 border-dotted border-black rounded-lg">
       <div className="p-5 flex flex-col gap-2">
         <AboutMe />
-        <hr className="border-dotted border-black w-125 mx-auto mt-2" />
-        <p className="font-bold text-2xl">Working exprience</p>
+        <hr className="border-dotted border-black w-full max-w-[500px] mx-auto mt-2" />
+        <p className="font-bold text-xl sm:text-2xl">Working exprience</p>
         <div>
-          <p>2023 - Now </p>
-          <p className="font-bold text-lg">Career Transition</p>
-          <p>
+          <p className="text-sm md:text-md">2023 - Now </p>
+          <p className="font-bold text-xl sm:text-2xl">Career Transition</p>
+          <p className="text-sm md:text-md">
             In 2023, I made a career change from my previous role as a
             kindergarten teacher to pursue my passion for web development.
             During this time, I focused on learning and building projects using
@@ -26,20 +26,22 @@ export default function Experience() {
             or on my Github.
           </p>
         </div>
-        <hr className="border-dotted border-black w-125 mx-auto mt-2" />
+        <hr className="border-dotted border-black w-full max-w-[500px] mx-auto mt-2" />
         <div>
           <p>2024 - Now </p>
           <p className="font-bold text-lg">ZŠ a MŠ Dolákova</p>
-          <p className="font-semibold">Kindergarten teaching assistant</p>
+          <p className="font-semibold text-sm md:text-md">
+            Kindergarten teaching assistant
+          </p>
           <ol>
-            <li>
+            <li className="text-sm md:text-md">
               ✨Assisting the teacher in planning and delivering lessons and
               activities.
             </li>
-            <li>
+            <li className="text-sm md:text-md">
               ✨Providing one-on-one support to children with additional needs.
             </li>
-            <li>
+            <li className="text-sm md:text-md">
               ✨Assisting with classroom decorations, such as cutting and
               decorating windows and displays
             </li>
@@ -51,16 +53,20 @@ export default function Experience() {
           <p className="font-bold text-lg">ZŠ a MŠ Dolákova</p>
           <p className="font-semibold">Kindergarten teacher</p>
           <ol>
-            <li>
+            <li className="text-sm md:text-md">
               ✨Planning and delivering age-appropriate lessons and activities.
             </li>
-            <li>
+            <li className="text-sm md:text-md">
               {`✨Supporting children's emotional, social, and cognitive
               development.`}
             </li>
-            <li>✨Ensuring a safe and nurturing environment</li>
-            <li>{`✨Communicating with parents about children's progress`}</li>
-            <li>✨Assisting children with personal care needs</li>
+            <li className="text-sm md:text-md">
+              ✨Ensuring a safe and nurturing environment
+            </li>
+            <li className="text-sm md:text-md">{`✨Communicating with parents about children's progress`}</li>
+            <li className="text-sm md:text-md">
+              ✨Assisting children with personal care needs
+            </li>
           </ol>
         </div>
         <hr className=" border-dotted border-black my-2 w-80 mx-auto" />
@@ -69,13 +75,19 @@ export default function Experience() {
           <p className="font-bold text-lg">FANY GASTROSERVIS s.r.o.</p>
           <p className="font-semibold">Shipping and billing clerk</p>
           <ol>
-            <li>✨Processing, creating, and printing sales documents.</li>
-            <li>✨Correcting and reviewing documents from employees.</li>
-            <li>
+            <li className="text-sm md:text-md">
+              ✨Processing, creating, and printing sales documents.
+            </li>
+            <li className="text-sm md:text-md">
+              ✨Correcting and reviewing documents from employees.
+            </li>
+            <li className="text-sm md:text-md">
               ✨Processing financial documents for the accounting department
             </li>
-            <li>✨Training new team members.</li>
-            <li>✨Handling complaints and issuing credit notes.</li>
+            <li className="text-sm md:text-md">✨Training new team members.</li>
+            <li className="text-sm md:text-md">
+              ✨Handling complaints and issuing credit notes.
+            </li>
           </ol>
         </div>
       </div>
